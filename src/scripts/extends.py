788badbefc18767e4083bbf3dc2124ec52588c72
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-11-20 13:19:13 +0100

    commented out unneeded scripts

---
+++ src/scripts/extends.py
@@ -0,0 +1,35 @@
+
+
+
+# Use cases
+# - Add objects to the list
+# - 
+# def modify_object_datas(digital_objects):
+#     """
+#     Custom function allows to modify digital objects data in memory.
+#     :param digital_objects: list of digital objects
+#     :return: list of digital objects 
+#     """
+
+#     filtered = []
+
+#     for digital_object in  digital_objects:
+#         # print(digital_object.db)
+#         if digital_object.db["baseMetadata"]["title"] == 'Alexei Jopich':
+#             filtered.append(digital_object)
+#             pass
+#         else:
+#             pass
+
+
+#     return filtered
+
+
+# # Completely erwrite what objects are being loaded
+# def load_digital_objects():
+#     """
+#     Custom function allows to build digital objects in memory.
+#     :return: list of digital objects
+#     """
+
+#     return []
